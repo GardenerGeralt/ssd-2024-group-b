52eb54578cffa280e0ee7d3573a2c233adbc34d4
NEONGASHMEN <ganesh2516822@gmail.com>
2024-03-06 21:14:24 +0100

    Correction units

--- Bipod_analytical_model/config.py
+++ Bipod_analytical_model/config.py
@@ -4,20 +4,21 @@
 import numpy as np
 
 # Tangential flexure
-la = 40*1e3
-Wa = 30*1e3
-ta = 2*1e3
+la = 40*1e-3
+Wa = 30*1e-3
+ta = 2*1e-3
 
 # Radial flexure
-lb = 40*1e3
-Wb = 30*1e3
-tb = 2*1e3
+lb = 40*1e-3
+Wb = 30*1e-3
+tb = 2*1e-3
 
 # Assem
-psi = 64*180/np.pi
-d = 20*1e3
-E = 80*1e9              # Aluminium
-M = 46                  # Mass of telescope
+psi = 64*np.pi/180
+d = 20*1e-3
+E = 69*1e9              # Aluminium
+M = 64                   # Mass of telescope
+D = 134.995*1e-3        # Diameter of dish
 
 
 
