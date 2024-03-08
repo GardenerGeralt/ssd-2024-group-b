8e8da2d6f393b33cd817f4fb0b80689a215429fa
NEONGASHMEN <ganesh2516822@gmail.com>
2024-03-08 15:09:11 +0100

    Minor corrections

--- Bipod_analytical_model/config.py
+++ Bipod_analytical_model/config.py
@@ -15,11 +15,13 @@ tb = 2*1e-3
 
 # Assem
 psi = 64*np.pi/180
-d = 40*1e-3
+#d = 40*1e-3            # value in FEM
+d = 20*1e-3             # value in paper
 E = 69*1e9              # Aluminium - youngs modulus
 G = 26*1e9              # Aluminium - shear modulus
 M = 64                  # Mass of telescope
-D = 134.995*1e-3        # Diameter of dish
+#D = 134.995*1e-3        # Diameter of dish
+D = 0.8                 # Diameter of dish (paper)
 
 
 
