aaf4e90417a862d674aad7da2835d76f2b45964e
NEONGASHMEN <ganesh2516822@gmail.com>
2024-03-08 14:32:50 +0100

    Add transformations and overall compliance matrix

--- Bipod_analytical_model/common_functions.py
+++ Bipod_analytical_model/common_functions.py
@@ -2,3 +2,48 @@ import numpy as np
 
 def torsion_constant(b, h):
     return (b*h**3/16)*(16/3 + 3.36*(h/b)*(1 - h**4/(12*b**4)))
+
+def rot_transform(theta, axis):
+    TF = np.matrix(np.zeros([3,3]))
+    if axis == "X":
+        TF[0,0] = 1
+        TF[1,1] = np.cos(theta)
+        TF[1,2] = -np.sin(theta)
+        TF[2,1] = np.sin(theta)
+        TF[2,2] = np.cos(theta)
+        return TF
+    elif axis == "Y":
+        TF[1,1] = 1
+        TF[0,0] = np.cos(theta)
+        TF[0,2] = np.sin(theta)
+        TF[2,0] = -np.sin(theta)
+        TF[2,2] = np.cos(theta)
+        return TF
+    elif axis == "Z":
+        TF[2,2] = 1
+        TF[0,0] = np.cos(theta)
+        TF[0,1] = -np.sin(theta)
+        TF[1,0] = np.sin(theta)
+        TF[1,1] = np.cos(theta)
+        return TF
+    else:
+        print("Invalid arguments")
+        return 0
+
+def lin_transform(t, l, r, b):                          # Duno where the x, y & z translation go in the transformation matrix, so I'm blindly coding atm
+    TF = np.matrix(np.zeros([3,3]))
+    TF[0,1] = t
+    TF[1,0] = l
+    TF[1,2] = r
+    TF[2,1] = b
+    return TF
+
+def compliance_transform(R, D):
+    TF = np.matrix(np.zeros([6,6]))
+    TF[0:3,0:3] = R
+    TF[3:6,0:3] = D*R
+    TF[3:6,3:6] = R
+    return TF
+
+#def matrix_inverse(M):
+
--- Bipod_analytical_model/main.py
+++ Bipod_analytical_model/main.py
@@ -4,6 +4,8 @@ from config import *
 from common_functions import *
 
 np.set_printoptions(linewidth=99)            # print 6x6 matrix properly
+DTR = 180/np.pi
+RTD = 1/DTR
 
 if la != lb:
     print("Need to derive anaytical sltn for assymmetrical bipod")
@@ -93,10 +95,43 @@ print("")
 print("Compliance matrix of single bipod:")
 print(Cp)
 
+Kp = Cp.I
+
+R1 = np.matrix(np.identity(3))
+#D1 = lin_transform(0,0,-d/2,-d/2)           # is it really d/D?? & lokks like translation in Z direction rather than X why???
+D1 = lin_transform(-D/2, -D/2, 0, 0)         # this seems more right but not exact why???
+R2 = rot_transform(120*DTR, "Y")
+D2 = lin_transform(D/4, -D/4, -D*np.sqrt(3)/4, D*np.sqrt(3)/4)
+R3 = rot_transform(-120*DTR, "Y")
+D3 = lin_transform(D/4, -D/4, np.sqrt(3)*D/4, -np.sqrt(3)*D/4)
+
+Ad1 = compliance_transform(R1, D1)
+Ad2 = compliance_transform(R2, D2)
+Ad3 = compliance_transform(R3, D3)
+K = Ad1*Kp*Ad1.I + Ad2*Kp*Ad2.I + Ad3*Kp*Ad3.I
+C = K.I
+
+print("")
+print("Compliance matrix of bipod system:")
+print(C)
 
 
+#---------------------- Resonance freq of the assembly ----------------------------#
 
+C41 = C[3,0]
+C63 = C[5,2]
+C52 = C[4,1]
 
+Fx = 1/(2*np.pi*np.sqrt(C41*M))
+Fy = 1/(2*np.pi*np.sqrt(C52*M))
+Fz = 1/(2*np.pi*np.sqrt(C63*M))
+
+print("")
+print("Resonance freqs of assembly")
+print(25*"-")
+print("Fx: "+str(Fx))
+print("Fy: "+str(Fy))
+print("Fz: "+str(Fz))
 
 
 
